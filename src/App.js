6746ce585d1d36cccc66b0bf1523b9bd7403887b
zwenger <ritortoalvaro93@gmail.com>
2019-05-30 12:51:20 -0300

    agregue la request de recipes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = '6e95c0e0';
+  const APP_KEY = 'bf67899300d58440929bc4c98ec950dd';
+  const url =`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect( () =>{
+    getRecipes();
+  }, []);
+
+const getRecipes = async () =>{
+  const response = await fetch('url'); // saca las comillas para que haga LA REQUEST
+  const data = await response.json();
+  console.log(data.hits);
+}
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input type="text" name="search" className="search-bar"/>
+        <button type="submit" className="search-button ">Search</button>
+      </form>
     </div>
   );
 }
